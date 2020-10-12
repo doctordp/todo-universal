d7e2953974c9dadebed8039445e504fe8abb3c46
yasmany1111 <yasmany1998@gmail.com>
2020-10-12 15:56:21 +0200

    Initial Commit

---
+++ src/app/todos/components/todo-list/todo-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-list',
+  styleUrls: [
+    './todo-list.component.scss',
+  ],
+  templateUrl: './todo-list.component.html',
+})
+export class TodosListComponent {}
